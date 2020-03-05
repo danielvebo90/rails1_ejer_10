1a427cd57bea615d6fc1ec0bfe641834f00a825f
danielvebo <danielvebo@gmail.com>
2020-03-04 20:21:35 -0500

    1 repo

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,31 @@
+class ExpensesController < ApplicationController
+  before_action :authenticate_user!
+
+  	def index
+
+      @expenses = Expense.order("date DESC")
+	    @expenses = @expenses.where("user_id = ?" , current_user.id)
+
+      if params[:concept].present?
+   		   expenses_temporal = Array.new
+          @expenses.each do |temp|
+     		if temp.concept.downcase.include? params[:concept].downcase
+     			expenses_temporal << temp
+     		end
+   		end
+          @expenses = expenses_temporal
+      end
+
+    	if params[:category_id].present?
+     		category = Category.find(params[:category_id])
+     		expenses_temporal = Array.new
+
+	     	@expenses.each do |temp|     		
+	     		if temp.category == category
+	     			expenses_temporal << temp
+	     		end
+     		end
+            @expenses = expenses_temporal
+    	end
+	end
+end
